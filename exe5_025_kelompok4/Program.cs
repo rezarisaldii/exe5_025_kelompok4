cb9382b0ff842f18b4f6db342d7bcecfabfa9746
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-12-23 15:57:05 +0700

    Add project files.

---
+++ exe5_025_kelompok4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe5_025_kelompok4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
